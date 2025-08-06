a2908ad4dfc1308cbbab30de70f3cb8366bc7df9
Mohamed Alouardi <157888329+TheCodeNoodle@users.noreply.github.com>
2025-08-07 00:36:55 +0100

    Add files via upload

--- fileOrganizer/main.py
+++ fileOrganizer/main.py
@@ -1,8 +1,20 @@
+import os
 from folders import make_DIR
 from organizer import move_files
+from undo import undo
+
+action = {}
 
 folder_link_input = input("Folder to Organize: ")
 link = folder_link_input.strip('"').replace("\\", "/")
 
+if not os.path.exists(link):
+    print("The path you entered does not exist.")
+    exit()
+
 make_DIR(link)
-move_files(link)
+move_files(link, action)
+
+undo_inp = input("Do you want to undo the file organization? (y/n): ").strip().lower()
+if undo_inp == 'y':
+    undo(action)
--- fileOrganizer/organizer.py
+++ fileOrganizer/organizer.py
@@ -2,6 +2,7 @@ import os
 import shutil
 from constants import *
 
+action = {}
 def get_unique_path(dest, filename):
     name, ext = os.path.splitext(filename)
     counter = 1
@@ -14,7 +15,7 @@ def get_unique_path(dest, filename):
 
     return new_path
 
-def move_files(path):
+def move_files(path, action):
     items = os.listdir(path)
 
     paths = {
@@ -25,38 +26,59 @@ def move_files(path):
         "Pictures": os.path.join(path, "Pictures"),
         "Music": os.path.join(path, "Music"),
         "Other": os.path.join(path, "Other"),
-    }
+    }#_/path/folder_name
 
     for item in items:
-        _, ext = os.path.splitext(item)
-        item_path = os.path.join(path, item)
+        _, ext = os.path.splitext(item) #splits extension from the name
+        item_path = os.path.join(path, item) #
 
         if os.path.isdir(item_path):
             continue
 
         ext = ext.lower()
-        moved = False
+        target_dir = None
 
         if ext in PROGRAM_EXTENSIONS:
-            shutil.move(item_path, get_unique_path(paths["Programs"], item))
-            moved = True
+            target_dir = paths["Programs"]
         elif ext in DOCUMENT_EXTENSIONS:
-            shutil.move(item_path, get_unique_path(paths["Documents"], item))
-            moved = True
+            target_dir = paths["Documents"]
         elif ext in ZIP_EXTENSIONS:
-            shutil.move(item_path, get_unique_path(paths["Zip"], item))
-            moved = True
+            target_dir = paths["Zip"]
         elif ext in VIDEO_EXTENSIONS:
-            shutil.move(item_path, get_unique_path(paths["Videos"], item))
-            moved = True
+            target_dir = paths["Videos"]
         elif ext in PICTURE_EXTENSIONS:
-            shutil.move(item_path, get_unique_path(paths["Pictures"], item))
-            moved = True
+            target_dir = paths["Pictures"]
         elif ext in MUSIC_EXTENSIONS:
-            shutil.move(item_path, get_unique_path(paths["Music"], item))
-            moved = True
+            target_dir = paths["Music"]
+        elif ext != "":
+            target_dir = paths["Other"]
+            
 
-        if not moved and ext != "":
-            shutil.move(item_path, get_unique_path(paths["Other"], item))
+        if target_dir:
+            unique_path = get_unique_path(target_dir, item)
+            shutil.move(item_path, unique_path)
+            action[item_path.replace("\\", "/")] = unique_path.replace("\\", "/")
 
     print("Files in the given destination were successfully Organized.")
+    print(f"Moved {len(action)} Files in total")
+    for bef_p, aft_p in action.items():
+        print(f"{bef_p} -> {aft_p}")
+
+def undo(path, action):
+    for bef_path, aft_path in action.items():
+        shutil.move(aft_path, bef_path)
+    paths = {
+        "Programs": os.path.join(path, "Programs"),
+        "Documents": os.path.join(path, "Documents"),
+        "Zip": os.path.join(path, "Zip"),
+        "Videos": os.path.join(path, "Videos"),
+        "Pictures": os.path.join(path, "Pictures"),
+        "Music": os.path.join(path, "Music"),
+        "Other": os.path.join(path, "Other"),
+    }
+
+    for i in range(len(paths)):
+        for path in paths:
+            if path:
+                
+    print("Undone!")
